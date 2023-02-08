191fdd24be480720760bbf448a5e6e7942b1d51d
g-80 <ghaith.ger@gmail.com>
2023-02-08 14:23:49 +0000

    add Product model

--- api/models.py
+++ api/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Test(models.Model):
     text = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=750, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    expiry = models.DateField()
