74275cda5fd67f40d060f92b9c460a10daca6266
g-80 <ghaith.ger@gmail.com>
2023-02-08 17:18:03 +0000

    Add validations and more constraints to Product model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
+import datetime
 
 # Create your models here.
 class Test(models.Model):
@@ -7,6 +10,18 @@ class Test(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=750, blank=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0.05)]) # accepts ints too. max price is 999.99
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     expiry = models.DateField()
+
+    def is_expiry_date_past(self):
+        if self.expiry < datetime.date.today(): return True
+        else: return False
+
+    def clean(self):
+        if (self.is_expiry_date_past()):
+            raise ValidationError("Expiry date cannot be in the past.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
