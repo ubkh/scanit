61cb1a302b8d8875771a2fb4f5b948c77be17f40
g-80 <ghaith.ger@gmail.com>
2023-02-18 18:13:31 +0000

    Implement navigation to 'StoreAddProducts'

--- frontend/scanit/src/navigation/screens/StoreAddItem.js
+++ frontend/scanit/src/navigation/screens/StoreAddItem.js
@@ -2,28 +2,60 @@ import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Button, Text } from "@rneui/base";
 import ContainerStyle from "../../styles/ContainerStyle";
+import { useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import BarCodeScanComponent from "../../components/BarCodeScanComponent";
+import StoreAddItemForm from "../../components/StoreAddItemForm";
 
-function StoreAddItem(props) {
+const Stack = createStackNavigator();
+
+function StoreAddItem() {
+  const navigation = useNavigation();
   return (
     <View style={ContainerStyle.container}>
       <Text>
-        Scan the item's barcode. Alternatively, you can enter the item's details
+        Scan the item's barcode. If the item already exists in the system,
+        details will be loaded. Alternatively, you can enter the item's details
         manually.
       </Text>
-      <Button onPress={() => ""} title="Scan barcode"></Button>
+      <Button
+        onPress={() => "Not implemented yet"}
+        title="Scan barcode"
+      ></Button>
       <Button
         containerStyle={{
           width: 200,
-          marginHorizontal: 10,
-          marginVertical: 10,
+          margin: 10,
         }}
         title="Enter details manually"
         type="clear"
-        titleStyle={{ color: "rgba(78, 116, 289, 1)" }}
+        onPress={() => navigation.navigate("StoreAddItemForm")}
       />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-export default StoreAddItem;
+function StoreAddItemStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="StoreAddItem"
+        component={StoreAddItem}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="BarCodeScanComponent"
+        component={BarCodeScanComponent}
+        options={{ headerTitle: "" }}
+      />
+      <Stack.Screen
+        name="StoreAddItemForm"
+        component={StoreAddItemForm}
+        options={{ headerTitle: "" }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default StoreAddItemStack;
