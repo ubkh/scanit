0b861bdbcbb7e849f7078592f51842d4a2b86a62
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2023-03-08 23:38:16 +0000

    Fix rendering errors for tests

--- frontend/scanit/__tests__/Basket.test.js
+++ frontend/scanit/__tests__/Basket.test.js
@@ -2,43 +2,103 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 import { Context } from '../context/GlobalContext';
 import Basket from '../app/(user)/(customer)/Basket';
+import { NativeBaseProvider } from 'native-base';
 
 describe('Basket', () => {
-  it('should display "Your basket is empty" when basketList is empty', () => {
-    const { getByText } = render(<Basket />);
-    expect(getByText('Your basket is empty')).toBeTruthy();
-  });
+  const basketList = [
+    {
+      data: '1234567890',
+      type: '512',
+      quantity: 2,
+    },
+    {
+      data: '0987654321',
+      type: '32',
+      quantity: 1,
+    },
+  ];
+  const setBasketList = jest.fn();
 
-  it('should display the number of products in the basket', () => {
-    const basketList = [
-      { data: '123456', type: '32', quantity: 2 },
-      { data: '654321', type: '512', quantity: 1 },
-    ];
-    const { getByText } = render(<Basket basketList={basketList} />);
-    expect(getByText('2 products')).toBeTruthy();
-  });
-
-  it('should call handleQuantityChange when quantity is changed', () => {
-    const basketList = [{ data: '123456', type: '32', quantity: 1 }];
-    const handleQuantityChange = jest.fn();
-    const { getByTestId } = render(
-      <Basket basketList={basketList} handleQuantityChange={handleQuantityChange} />
+  it('renders empty basket message when basket is empty', () => {
+    const { getByText } = render(
+    <NativeBaseProvider>
+      <Context.Provider value={{ basketList: [], setBasketList }}>
+        <Basket />
+      </Context.Provider>
+    </NativeBaseProvider>,
     );
-    const quantityInput = getByTestId('numeric-input-0');
-    fireEvent.changeText(quantityInput, '2');
-    expect(handleQuantityChange).toHaveBeenCalledWith(0, 2);
+    expect(getByText('Your basket is empty')).toBeDefined();
   });
 
-  it('should call removeItem when remove button is pressed', () => {
-    const basketList = [
-        { data: '123456', type: '32', quantity: 1 },
-        { data: '654321', type: '512', quantity: 2 },
-    ];
-    const removeItem = jest.fn();
-    const { getByTestId } = render(<Basket basketList={basketList} removeItem={removeItem} />);
-    const removeButton = getByTestId('remove-button-1');
+  it('renders basket items when basket is not empty', () => {
+    const { getByText, getByTestId } = render(
+    <NativeBaseProvider>
+      <Context.Provider value={{ basketList, setBasketList }}>
+        <Basket />
+      </Context.Provider>
+    </NativeBaseProvider>,
+    );
+    expect(getByText(`${basketList.length} products`)).toBeDefined();
+    expect(getByTestId('numeric-input-0')).toBeDefined();
+    expect(getByTestId('numeric-input-1')).toBeDefined();
+    expect(getByTestId('remove-button-0')).toBeDefined();
+    expect(getByTestId('remove-button-1')).toBeDefined();
+  });
+
+  it('calls handleQuantityChange when numeric input value is changed', () => {
+    const { getByTestId } = render(
+    <NativeBaseProvider>
+      <Context.Provider value={{ basketList, setBasketList }}>
+        <Basket />
+      </Context.Provider>
+    </NativeBaseProvider>,
+    );
+    const numericInput = getByTestId('numeric-input-0');
+    fireEvent.changeText(numericInput, '3');
+    expect(setBasketList).toHaveBeenCalledWith([
+      {
+        data: '1234567890',
+        type: '512',
+        quantity: 3,
+      },
+      {
+        data: '0987654321',
+        type: '32',
+        quantity: 1,
+      },
+    ]);
+  });
+
+  it('calls removeItem when remove button is pressed', () => {
+    const { getByTestId } = render(
+    <NativeBaseProvider>
+      <Context.Provider value={{ basketList, setBasketList }}>
+        <Basket />
+      </Context.Provider>
+    </NativeBaseProvider>,
+    );
+    const removeButton = getByTestId('remove-button-0');
     fireEvent.press(removeButton);
-    expect(removeItem).toHaveBeenCalledWith(1);
+    expect(setBasketList).toHaveBeenCalledWith([
+      {
+        data: '0987654321',
+        type: '32',
+        quantity: 1,
+      },
+    ]);
+  });
+
+  it('shows alert when quantity input is less than 1', () => {
+    const { getByTestId } = render(
+    <NativeBaseProvider>
+      <Context.Provider value={{ basketList, setBasketList }}>
+        <Basket />
+      </Context.Provider>
+    </NativeBaseProvider>,
+    );
+    const numericInput = getByTestId('numeric-input-0');
+    fireEvent.changeText(numericInput, '0');
+    expect(setBasketList).not.toHaveBeenCalled();
+    expect(getByText('Quantity must be 1 or more!')).toBeDefined();
   });
 });
-
