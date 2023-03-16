262f3107ff3a611339fd9874067b4c8a1707c706
g-80 <ghaith.ger@gmail.com>
2023-03-16 20:01:13 +0000

    Update Product model to include the retailer

--- api/models.py
+++ api/models.py
@@ -10,54 +10,54 @@ import datetime
 
 
 class CustomUserManager(BaseUserManager):
-	def create_user(self, email, password):
-		if not email:
-			raise ValueError('A user email is needed.')
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError('A user email is needed.')
 
-		if not password:
-			raise ValueError('A user password is needed.')
+        if not password:
+            raise ValueError('A user password is needed.')
 
-		email = self.normalize_email(email)
-		user = self.model(email=email)
-		user.set_password(password)
-		user.save()
-		return user
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
 
-	def create_superuser(self, email, password, username=None):
-		if not email:
-			raise ValueError('A user email is needed.')
+    def create_superuser(self, email, password, username=None):
+        if not email:
+            raise ValueError('A user email is needed.')
 
-		if not password:
-			raise ValueError('A user password is needed.')
+        if not password:
+            raise ValueError('A user password is needed.')
 
-		user = self.create_user(email, password)
-		user.is_superuser = True
-		user.is_staff = True
-		user.save()
-		return user
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-	phone_regex = RegexValidator(regex=r'^\0?1?\d{11}$', message="Phone number must have 11 digits.")
+    phone_regex = RegexValidator(regex=r'^\0?1?\d{11}$', message="Phone number must have 11 digits.")
+    
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    number = models.CharField(validators=[phone_regex], max_length=11, blank=True)
+    store_address = models.CharField(max_length=100, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_retailer = models.BooleanField(default=False)
+    # retailer_id= models.CharField(max_length=8, blank=True)
+    verification_code = models.CharField(max_length=6)
+    is_verified = models.BooleanField(default=False)
+    date_joined = models.DateField(auto_now_add=True)
+    USERNAME_FIELD = 'email'
+    objects = CustomUserManager()
 
-	user_id = models.AutoField(primary_key=True)
-	email = models.EmailField(max_length=100, unique=True)
-	first_name = models.CharField(max_length=32)
-	last_name = models.CharField(max_length=32)
-	number = models.CharField(validators=[phone_regex], max_length=11, blank=True)
-	store_address = models.CharField(max_length=100, blank=True, null=True)
-	is_active = models.BooleanField(default=True)
-	is_staff = models.BooleanField(default=False)
-	is_retailer = models.BooleanField(default=False)
-	# retailer_id= models.CharField(max_length=8, blank=True)
-	verification_code = models.CharField(max_length=6)
-	is_verified = models.BooleanField(default=False)
-	date_joined = models.DateField(auto_now_add=True)
-	USERNAME_FIELD = 'email'
-	objects = CustomUserManager()
-
-	def __str__(self):
-		return f'{self.first_name} {self.last_name}'
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 class Test(models.Model):
     text = models.CharField(max_length=100)
@@ -69,17 +69,27 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     expiry = models.DateField()
     barcode = models.CharField(max_length=20)
-    # retailer = models.ForeignKey(on_delete= models.CASCADE)
+    retailer = models.ForeignKey(User, on_delete= models.CASCADE)
 
     def is_expiry_date_past(self):
-        if self.expiry < datetime.date.today(): return True
-        else: return False
+        if self.expiry < datetime.date.today():
+            return True
+        return False
+
+    def is_invalid_barcode(self):
+        if (all(char.isdigit() for char in self.barcode)):
+            return False
+        return True
 
     def clean(self):
         if (self.is_expiry_date_past()):
             raise ValidationError("Expiry date cannot be in the past.")
+        if (self.is_invalid_barcode()):
+            raise ValidationError("Invalid barcode")
 
     def save(self, *args, **kwargs):
+        # even though full_clean() should call clean_fields() before clean(), it does not, and expiry date is not checked if valid
+        self.clean_fields()
         self.full_clean()
         super().save(*args, **kwargs)
 
--- api/utils.py
+++ api/utils.py
@@ -1,13 +1,22 @@
 from django.conf import settings
 from datetime import datetime, timedelta
 import jwt
+from django.contrib.auth import get_user_model
 
 
 def generate_access_token(user):
-	payload = {
-		'user_id': user.user_id,
-		'exp': datetime.utcnow() + timedelta(days=1, minutes=0),
-		'iat': datetime.utcnow(),
-	}
-	access_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-	return access_token
+    payload = {
+        'user_id': user.user_id,
+        'exp': datetime.utcnow() + timedelta(days=1, minutes=0),
+        'iat': datetime.utcnow(),
+    }
+    access_token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    return access_token
+
+def get_logged_in_user(user_token):
+    payload = jwt.decode(user_token, settings.SECRET_KEY, algorithms=['HS256'])
+    user_model = get_user_model()
+    user = user_model.objects.filter(user_id=payload['user_id']).first()
+    if user:
+        return user
+    return None
